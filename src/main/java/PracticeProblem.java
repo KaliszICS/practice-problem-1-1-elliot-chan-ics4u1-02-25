8022c52b0325027a54a322ad3fae4579bcee4e48
Elliot Chan <elliot.chan@student.tdsb.on.ca>
2025-03-15 02:04:08 +0000

    Finish practice problem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,4 +3,29 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int sum2D(int[][] array) {
+		int sum = 0;
+		for(int row = 0; row < array.length; row++) {
+			for(int column = 0; column < array[row].length; column++) {
+				sum+=array[row][column];
+			}
+		}
+		return sum;
+	}
+	public static int sumRow(int[][] array, int rowNum) {
+		int sum = 0;
+		for(int column = 0; column < array[rowNum].length; column++) {
+			sum+=array[rowNum][column];
+		}
+		return sum;
+	}
+	public static int sumColumn(int[][] array, int columnNum) {
+		int sum = 0;
+		for(int row = 0; row < array.length; row++) {
+					sum+=array[row][columnNum];
+			
+		}
+		return sum;
+	}
+	
 }
